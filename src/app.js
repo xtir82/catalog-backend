58b6d87a3a0a0a220004ba78ca7178c4a0054751
Andherson Castro <agcl82@gmail.com>
2024-07-18 20:11:17 -0400

    Adding handlebars and setting it

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 import express from "express";
-import ProductManager from "./Class/productManager.js";
 import { __dirname } from "./utils.js";
+import handlebars from "express-handlebars";
 
+//Routes
 import CartRoute from './routes/cart.router.js';
 import ProductRoute from './routes/product.router.js';
 
-//Middleware
 const app = express();
 const port = 8080;
 
+//Template Engine
+app.engine('handlebars', handlebars.engine()); //Inicializamos el template engine, en este caso handlebars
+app.set('views', __dirname + '/views'); //Configuramos la ruta de las views
+app.set('view engine', 'handlebars');
+
+//Middleware
 app.use(express.json()); //body-parse
 app.use(express.urlencoded({ extended: true }));
 
@@ -16,5 +22,5 @@ app.use('/api/product',  ProductRoute);
 app.use('/api/cart',  CartRoute);
 
 app.listen(port, () => {
-    console.log("Servidor iniciado")
+    console.log("Servidor iniciado en puerto " + port)
 })
